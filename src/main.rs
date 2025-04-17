721fb1b11d552db4728952b7f18cf77d08abec7d
Dr-42 <royspandan1@gmail.com>
2025-04-18 01:46:51 +0530

    Scan network

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use audio_device::AudioDevice;
 use audio_source::AudioSource;
 use lazy_static::lazy_static;
 use local_ip_address::local_ip;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::str::FromStr;
 
 use types::{IpAddress, MacAddress};
@@ -19,27 +20,49 @@ lazy_static! {
     };
 }
 
+fn scan_network() -> Vec<String> {
+    // Scanning from 192.168.1.1 to 192.168.1.255
+    (0..255)
+        .into_par_iter()
+        .filter_map(|i| {
+            let ip = format!("192.168.1.{}", i);
+            println!("Pinging {}", ip);
+            let command = format!("ping -c 1 -W 0.5 {}", ip);
+            let resp = std::process::Command::new("sh")
+                .arg("-c")
+                .arg(&command)
+                .output();
+            let available = resp.is_ok();
+            let available = available && resp.unwrap().status.success();
+            if available { Some(ip) } else { None }
+        })
+        .collect::<Vec<String>>()
+}
+
 fn main() {
     println!("Hello, {}!", *SERVER_IP_ADDRESS);
-    let lappy = AudioSource::new(
-        "Lappy".to_string(),
-        "spandan".to_string(),
-        IpAddress::from_str("192.168.1.40").unwrap(),
-    );
-    let homesrv = AudioSource::new(
-        "Homesrv".to_string(),
-        "spandan".to_string(),
-        IpAddress::from_str("192.168.1.37").unwrap(),
-    );
-
-    let speaker = AudioDevice::new(
-        "Speaker".to_string(),
-        MacAddress::from_str("F4:4E:FD:0A:DC:4C").unwrap(),
-    );
-
-    lappy.disconnect(&speaker);
-    homesrv.connect(&speaker);
-    std::thread::sleep(std::time::Duration::from_secs(20));
-    homesrv.disconnect(&speaker);
-    lappy.connect(&speaker);
+    scan_network().iter().for_each(|ip| {
+        println!("{}", ip);
+    });
+    // let lappy = AudioSource::new(
+    //     "Lappy".to_string(),
+    //     "spandan".to_string(),
+    //     IpAddress::from_str("192.168.1.40").unwrap(),
+    // );
+    // let homesrv = AudioSource::new(
+    //     "Homesrv".to_string(),
+    //     "spandan".to_string(),
+    //     IpAddress::from_str("192.168.1.37").unwrap(),
+    // );
+    //
+    // let speaker = AudioDevice::new(
+    //     "Speaker".to_string(),
+    //     MacAddress::from_str("F4:4E:FD:0A:DC:4C").unwrap(),
+    // );
+    //
+    // lappy.disconnect(&speaker);
+    // homesrv.connect(&speaker);
+    // std::thread::sleep(std::time::Duration::from_secs(20));
+    // homesrv.disconnect(&speaker);
+    // lappy.connect(&speaker);
 }
